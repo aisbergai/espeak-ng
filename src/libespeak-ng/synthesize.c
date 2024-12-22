907981d37e9fe0d9d4ea182d43a5be3d61d7b0ce
Adrian Punga <adrian.punga@gmail.com>
2024-12-22 22:57:04 +0000

    Clean space

--- src/libespeak-ng/synthesize.c
+++ src/libespeak-ng/synthesize.c
@@ -1147,7 +1147,6 @@ int Generate(PHONEME_LIST *phoneme_list, int *n_ph, bool resume)
 
 	if (option_phoneme_events & espeakINITIALIZE_PHONEME_IPA)
 		use_ipa = 1;
-	
 #if USE_MBROLA
 	if (mbrola_name[0] != 0)
 		return MbrolaGenerate(phoneme_list, n_ph, resume);
